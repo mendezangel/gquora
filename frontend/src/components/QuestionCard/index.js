8ea51d9ee6f1cee0be52e1b8cb624a945c7c114b
mendezangel <id.angelmendez@gmail.com>
2022-04-06 15:54:07 -0700

    solved my bug

--- frontend/src/components/QuestionCard/index.js
+++ frontend/src/components/QuestionCard/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getQuestion } from '../../store/question';
 import { Link } from 'react-router-dom';
+import Navigation from '../Navigation';
 
 export default function QuestionCard() {
   const dispatch = useDispatch();
@@ -12,28 +13,31 @@ export default function QuestionCard() {
   }, [dispatch]);
   console.log(questions);
   return (
-    <div className='questions-container'>
-      {questions.map(question => {
-        return (
-          <div className='card-container' key={question.id}>
-            <div className='user-title-container'>
-              <div className='user-container'>
-                <Link to={`/users/${question.ownerId}`}>
-                  <h2>{question.User.username}</h2>
-                </Link>
+    <>
+      <Navigation />
+      <div className='questions-container'>
+        {questions?.map(question => {
+          return (
+            <div className='card-container' key={question.id}>
+              <div className='user-title-container'>
+                <div className='user-container'>
+                  <Link to={`/users/${question.ownerId}`}>
+                    <p>{question.User.username}</p>
+                  </Link>
+                </div>
+                <div className='title-container'>
+                  <Link to={`/questions/${question.id}`}>
+                    <h3>{question.title}</h3>
+                  </Link>
+                </div>
               </div>
-              <div className='title-container'>
-                <Link to={`/questions/${question.id}`}>
-                  <h3>{question.title}</h3>
-                </Link>
+              <div className='question-body'>
+                <p className='question-body-text'>{question.description}</p>
               </div>
             </div>
-            <div className='question-body'>
-              <p className='question-body-text'>{question.description}</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   )
 }
